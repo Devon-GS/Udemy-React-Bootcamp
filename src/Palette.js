b2fbb659a7bb0670110e5fd522a4ac8ece490185
Devon <devon@example.com>
2020-01-16 16:16:48 +0200

    Add Select and Material UI

--- src/Palette.js
+++ src/Palette.js
@@ -1,36 +1,37 @@
 import React, { Component } from 'react';
-import ColorBox from "./ColorBox";
-import Navbar from "./Navbar";
-import "./Palette.css";
+import ColorBox from './ColorBox';
+import Navbar from './Navbar';
+import './Palette.css';
 
 class Palette extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { level: 500 };
-        this.changeLevel = this.changeLevel.bind(this);
-    }
+	constructor(props) {
+		super(props);
+		this.state = { level: 500, format: "hex" };
+		this.changeLevel = this.changeLevel.bind(this);
+		this.changeFormat = this.changeFormat.bind(this);
+	}
 
-    changeLevel(level) {
-        this.setState({ level });
-    }
+	changeLevel(level) {
+		this.setState({ level });
+	}
 
-    render() {
-        const { colors } = this.props.palette;
-        const { level } = this.state;
-        const colorBoxes = colors[level].map(color => (
-            <ColorBox background={color.hex} name={color.name} />
-        ));
-        return (
-            <div className="Palette">
-                <Navbar level={level} changeLevel={this.changeLevel} />
-                {/* Navbar goes here */}
-                <div className="Palette-colors">
-                    {colorBoxes}
-                </div>
-                {/* footer eventually */}
-            </div>
-        );
-    }
+	changeFormat(val) {
+		this.setState({ format: val })
+	}
+
+	render() {
+		const { colors } = this.props.palette;
+		const { level, format } = this.state;
+		const colorBoxes = colors[level].map((color) => <ColorBox background={color[format]} name={color.name} />);
+		return (
+			<div className="Palette">
+				<Navbar level={level} changeLevel={this.changeLevel} handleChange={this.changeFormat} />
+				{/* Navbar goes here */}
+				<div className="Palette-colors">{colorBoxes}</div>
+				{/* footer eventually */}
+			</div>
+		);
+	}
 }
 
 export default Palette;
